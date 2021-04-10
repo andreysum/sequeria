81549ddb11a4180edf7b57b9868d02bd5565facd
Andrey Sumtsov <felix2k@tut.by>
2021-04-10 14:19:49 +0300

    + simple paste functionality

--- src/com/github/andreysum/sequeria/action/GenerateSequence.java
+++ src/com/github/andreysum/sequeria/action/GenerateSequence.java
@@ -2,14 +2,14 @@ package com.github.andreysum.sequeria.action;
 
 import com.intellij.codeInsight.actions.MultiCaretCodeInsightAction;
 import com.intellij.codeInsight.actions.MultiCaretCodeInsightActionHandler;
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorModificationUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
-
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,7 +25,6 @@ public class GenerateSequence extends MultiCaretCodeInsightAction {
                 final PsiDocumentManager documentManager = PsiDocumentManager.getInstance(project);
                 Document document = editor.getDocument();
                 documentManager.commitDocument(document);
-                EditorModificationUtil.insertStringAtCaret(editor, "Generated!");
             }
         };
     }
